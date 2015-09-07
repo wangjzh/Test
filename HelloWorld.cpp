514105f088126df8c2e06c3a4ecb50aecf50c6ee
wangjzh <wangjzh_1@163.com>
2015-09-07 16:24:37 +0800

    add HelloWorld.cpp

---
+++ HelloWorld.cpp
@@ -0,0 +1,14 @@
+/*************************************************************************
+    > File Name: HelloWorld.cpp
+    > Author: wangjzh_1
+    > Mail: wangjzh_1@163.com 
+    > Created Time: 2015年09月07日 星期一 16时23分04秒
+ ************************************************************************/
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	cout << "HelloWorld!" << endl;
+}
